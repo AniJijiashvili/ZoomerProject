bfed93519058c153a870e057f832ade9418243ad
NiniGogoladze23 <nini.gogoladze.231@gmail.com>
2024-10-10 16:53:46 +0400

    --loop on sections --visual refinments

--- template_script.js
+++ template_script.js
@@ -17,80 +17,72 @@ document.querySelectorAll(".next").forEach((button, index) => {
   });
 });
 
-// Fetching and adding products to the eager carousel
-fetch("products.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const carousel = document.querySelector(".product-carousel.eager");
-    data.eager.forEach((product) => {
-      const productCard = document.createElement("div");
-      productCard.className = "product-card";
-      // Check if previousPrice exists
-      const priceHTML = product.previousPrice
-        ? `<h4 class="product-new-price">${product.price} ₾ <span class="previous-price">${product.previousPrice} ₾</span></h4>`
-        : `<h4 class="product-price">${product.price} ₾</h4>`;
+// List of carousel types and their corresponding JSON keys
+const carouselTypes = [
+  "eager",
+  "discover",
+  "new",
+  "tvs",
+  "selfcare",
+  "buds",
+  "headphones",
+  "laptops",
+  "consoles",
+  "caraccessories",
+  "smarthome",
+  "topproducts",
+];
 
-      // Check if labelText is 'BEST PRICE' and add a label if true
-      const labelHTML =
-        product.labelText === "BEST PRICE"
-          ? `<div class="best-price-label">${product.labelText}</div>`
-          : "";
+// Fetch and add products to the carousels
+carouselTypes.forEach((carouselType) => {
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const carousel = document.querySelector(
+        `.product-carousel.${carouselType}`
+      );
+      data[carouselType].forEach((product) => {
+        const productCard = document.createElement("div");
+        productCard.className = "product-card";
 
-      productCard.innerHTML = `
-            ${labelHTML}
-            <img src="${product.imageUrl}" alt="${
-        product.name
-      }" class="product-image">
-            <div class="product-card--info">
-              ${priceHTML}
-              <p class="product-installment">თვეში: <span>${Math.round(
-                product.price / 36
-              )}</span> <span> ₾ </span>-დან</p>
-              <a href="${product.link}" class="product-link">${product.name}</a>
-            </div>
-            <div class="product-actions">
-                <button class="compare-btn"><img src="/icons/compare-card.svg" alt="card-icon"></button>
-                <button class="add-cart-btn"><img src="/icons/cart-button.svg" alt="btn-img"> <span> დამატება </span></button>
-            </div>
-        `;
-      carousel.appendChild(productCard);
+        // Check if previousPrice exists
+        const priceHTML = product.previousPrice
+          ? `<h4 class="product-new-price">${product.price} ₾ <span class="previous-price">${product.previousPrice} ₾</span></h4>`
+          : `<h4 class="product-price">${product.price} ₾</h4>`;
+
+        // Check if labelText is 'BEST PRICE' or 'NEW' and add a label if true
+        const labelHTML =
+          product.labelText === "BEST PRICE" || product.labelText === "NEW"
+            ? `<div class="product-text-label" title="${product.name}">${product.labelText}</div>`
+            : product.labelText === "Gifts"
+            ? `<div class="product-text-label" title="${
+                product.name
+              }">${product.labelText.toUpperCase()}</div>
+               <div class="gifts-label-icon" title="${
+                 product.name
+               }"><img src="/icons/gift-box.svg" alt="gift-icon"></div>`
+            : "";
+
+        productCard.innerHTML = `
+              ${labelHTML}
+              <img src="${product.imageUrl}" alt="${
+          product.name
+        }" class="product-image" title="${product.name}">
+              <div class="product-card--info">
+                ${priceHTML}
+                <p class="product-installment">თვეში: <span>${Math.round(
+                  product.price / 36
+                )}</span> <span> ₾ </span>-დან</p>
+                <a href="${product.link}" class="product-link"title="${
+          product.name
+        }">${product.name}</a>
+              </div>
+              <div class="product-actions">
+                  <button class="compare-btn"><img src="/icons/compare-card.svg" alt="card-icon"></button>
+                  <button class="add-cart-btn"><img src="/icons/cart-button.svg" alt="btn-img"> <span> დამატება </span></button>
+              </div>
+          `;
+        carousel.appendChild(productCard);
+      });
     });
-  });
-
-// Fetching and adding products to the discover carousel
-fetch("products.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const carousel = document.querySelector(".product-carousel.discover");
-    data.discover.forEach((product) => {
-      const productCard = document.createElement("div");
-      productCard.className = "product-card";
-      // Check if previousPrice exists
-      const priceHTML = product.previousPrice
-        ? `<h4 class="product-new-price">${product.price} ₾ <span class="previous-price">${product.previousPrice} ₾</span></h4>`
-        : `<h4 class="product-price">${product.price} ₾</h4>`;
-      // Check if labelText is 'BEST PRICE' and add a label if true
-      const labelHTML =
-        product.labelText === "BEST PRICE"
-          ? `<div class="best-price-label">${product.labelText}</div>`
-          : "";
-      productCard.innerHTML = `
-            ${labelHTML}
-            <img src="${product.imageUrl}" alt="${
-        product.name
-      }" class="product-image">
-            <div class="product-card--info">
-              ${priceHTML}
-              <p class="product-installment">თვეში: <span>${Math.round(
-                product.price / 36
-              )}</span> <span> ₾ </span>-დან</p>
-              <a href="${product.link}" class="product-link">${product.name}</a>
-            </div>
-            <div class="product-actions">
-                <button class="compare-btn"><img src="/icons/compare-card.svg" alt="card-icon"></button>
-                <button class="add-cart-btn"><img src="/icons/cart-button.svg" alt="btn-img"> <span> დამატება </span></button>
-            </div>
-        `;
-      carousel.appendChild(productCard);
-    });
-  });
+});
