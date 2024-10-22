d8f5ee04c91a194b6373396a311dbc982df9c5c2
Nestani Datunashvili <nestani.datunashvili@gmail.com>
2024-10-22 21:39:10 +0400

    header css

--- header.js
+++ header.js
@@ -4,9 +4,7 @@ const popupContainer = document.getElementById("popup_container");
 const overlay = document.getElementById("overlay");
 const popupContainerBox = document.getElementById("popup");
 const openCartButton = document.getElementById("open_cart");
-const openCountyOptions = document.getElementsByClassName(
-  "number_input--index"
-)[0];
+const openCountyOptions = document.getElementById("number_input--index");
 const countyContainer = document.getElementById("country_list--container");
 const selectedCounty = document.getElementById("deal_index");
 const languageContainer = document.getElementById("laguage");
@@ -23,12 +21,14 @@ window.addEventListener("scroll", () => {
   const headerBar = document.querySelector("header");
   const navigation = document.getElementById("header__details");
 
-  if (window.scrollY > 110) {
-    headerBar.classList.add("active");
-    navigation.style.display = "none";
-  } else {
-    headerBar.classList.remove("active");
-    navigation.style.display = "flex";
+  if (window.innerWidth > 1023) {
+    if (window.scrollY > 120) {
+      headerBar.classList.add("active");
+      navigation.style.display = "none";
+    } else {
+      headerBar.classList.remove("active");
+      navigation.style.display = "flex";
+    }
   }
 });
 
@@ -42,7 +42,8 @@ function handlePopupOpen() {
     handlePopupClose();
   });
   handleDropdownCountries();
-  handlePopupContent();
+  handleAutorization();
+  // handlePopupContent();
 }
 
 function handlePopupClose() {
@@ -56,12 +57,78 @@ function handlePopupClose() {
 // // handleAutorization
 
 function handleAutorization() {
+  const radioButton = document.getElementById("radio_rules--container");
+  const mailBox = document.getElementById("mail_box");
+  const numberBox = document.getElementById("number_box");
+  const indexContainer = document.getElementById("number_input--index");
+  const inputContainer = document.getElementById("number_input--container");
+  const popupInputs = document.getElementById("popup_inputs");
+
   redBottom.style.left = "0px";
   redBottom.style.right = "200px";
   autorizationContainer.style.display = "flex";
-  const radioButton = document.getElementById("radio_rules--container");
-
+  countyContainer.classList.remove("active");
   radioButton.style.display = "none";
+
+  let handleAutorizationType = () => {
+    mailBox.addEventListener("click", () => {
+      mailBox.classList.add("active");
+      numberBox.classList.remove("active");
+
+      indexContainer.style.display = "none";
+      inputContainer.style.display = "flex";
+      inputContainer.style.flexDirection = "column";
+
+      popupInputs.innerHTML = `
+      <div class="number_input-field">
+                  <input
+                    class="popup_input"
+                    id="mail_input"
+                    type="password"
+                    placeholder=" "
+                  />
+                  <label for="">ელფოსტა</label>
+                </div>
+                <div class="number_input-field">
+                  <input
+                    class="popup_input"
+                    id="email_input"
+                    type="password"
+                    placeholder=" "
+                  />
+                  <label for="">პაროლი</label>
+                  <img
+                    src="./assets/header/hiddenPassword.svg"
+                    alt="passwrd_hide"
+                    id="password_hide"
+                  />
+                </div>
+      `;
+
+      const handleShowPassword = () => {
+        const hideImg = document.getElementById("password_hide");
+        hideImg.addEventListener("click", () => {
+          hideImg.src = "./assets/header/showPassword.svg";
+        });
+      };
+    });
+
+    numberBox.addEventListener("click", () => {
+      numberBox.classList.add("active");
+      mailBox.classList.remove("active");
+      indexContainer.style.display = "flex";
+      inputContainer.style.display = "grid";
+
+      popupInputs.innerHTML = `
+      <div class="number_input-field">
+      <input  class="popup_input" id="number_input" type="text" placeholder=" " />
+      <label for="">ტელეფონის ნომერი</label>
+    </div>
+      `;
+    });
+  };
+
+  handleAutorizationType();
 }
 
 // // registration field
@@ -69,6 +136,7 @@ function handleAutorization() {
 function handleRegistration() {
   const radioButton = document.getElementById("radio_rules--container");
   const popupBtn = document.getElementById("popup_btn");
+  countyContainer.classList.remove("active");
 
   redBottom.style.left = "200px";
   autorizationContainer.style.display = "none";
@@ -179,7 +247,6 @@ async function handleDropdownCountries() {
       searchValue.value = "";
       handleDropdownCountries();
     });
-
     countriesList.appendChild(countryOption);
 
     return { countryOption, countryName };
@@ -249,84 +316,132 @@ async function displaySearchValue() {
   searchInput.addEventListener("keyup", async () => {
     const searchValue = searchInput.value.toLowerCase();
     const inputForm = document.getElementById("search_conteiner");
-    const searchResult = document.createElement("div");
-    searchResult.classList.add("search_result");
-    // searchResult.id = "search_result"; // Assign an ID for later reference
 
-    const searchResultTitle = document.createElement("div");
-    searchResultTitle.classList.add("search_result--title");
+    if (searchValue.length > 0) {
+      const overlaySearch = document.getElementById("search_result--overlay");
+      overlaySearch.classList.add("active");
+      const searchResult = document.createElement("div");
+      searchResult.classList.add("search_result");
 
-    const filterCategory = document.createElement("div");
-    filterCategory.textContent = "გაფილტრე კატეგორია";
+      const searchResultTitle = document.createElement("div");
+      searchResultTitle.classList.add("search_result--title");
 
-    const categoryAnchor = document.createElement("a");
-    categoryAnchor.href = "./navigation.html";
+      const filterCategory = document.createElement("div");
+      filterCategory.textContent = "გაფილტრე კატეგორია";
 
-    const allCategory = document.createElement("div");
-    allCategory.textContent = "ყველას ნახვა";
-    allCategory.classList.add("category");
+      const categoryAnchor = document.createElement("a");
+      categoryAnchor.href = "./navigation.html";
 
-    // Append elements
-    categoryAnchor.appendChild(allCategory);
-    searchResultTitle.appendChild(filterCategory);
-    searchResultTitle.appendChild(categoryAnchor);
-    searchResult.appendChild(searchResultTitle);
-    inputForm.appendChild(searchResult);
+      const allCategory = document.createElement("div");
+      allCategory.textContent = "ყველას ნახვა";
+      allCategory.classList.add("category");
 
-    document.addEventListener("click", (event) => {
-      if (
-        searchResult &&
-        !inputForm.contains(event.target) &&
-        event.target !== searchInput
-      ) {
-        searchResult.remove();
+      // Append elements
+      categoryAnchor.appendChild(allCategory);
+      searchResultTitle.appendChild(filterCategory);
+      searchResultTitle.appendChild(categoryAnchor);
+      searchResult.appendChild(searchResultTitle);
+      inputForm.appendChild(searchResult);
+
+      document.addEventListener("click", (event) => {
+        if (
+          searchResult &&
+          !inputForm.contains(event.target) &&
+          event.target !== searchInput
+        ) {
+          searchResult.remove();
+          overlaySearch.classList.remove("active");
+          searchValue = "";
+        }
+      });
+
+      const results = [];
+
+      for (const key in data) {
+        data[key].forEach((item) => {
+          if (item.name.toLowerCase().includes(searchValue)) {
+            results.push(item);
+          }
+        });
       }
-    });
 
-    const results = [];
+      if (results.length > 0) {
+        results.forEach((item) => {
+          const resultDetails = document.createElement("div");
+          resultDetails.classList.add("search_result--details");
 
-    for (const key in data) {
-      data[key].forEach((item) => {
-        if (item.name.toLowerCase().includes(searchValue)) {
-          results.push(item);
-        }
-      });
-    }
+          const resultDetailsContainer = document.createElement("div");
+          resultDetailsContainer.classList.add("search_details--container");
 
-    if (results.length > 0) {
-      results.forEach((item) => {
-        const resultDetails = document.createElement("div");
-        resultDetails.classList.add("search_result--details");
+          const resultImg = document.createElement("img");
+          resultImg.classList.add("result_img");
+          resultImg.src = item.imageUrl;
 
-        const resultDetailsContainer = document.createElement("div");
-        resultDetailsContainer.classList.add("search_details--container");
+          const resultDetailsContent = document.createElement("div");
+          resultDetailsContent.classList.add("search_details--content");
 
-        const resultImg = document.createElement("img");
-        resultImg.classList.add("result_img");
-        resultImg.src = item.imageUrl;
+          const resultDetailsTitle = document.createElement("div");
+          resultDetailsTitle.classList.add("search_details--title");
+          resultDetailsTitle.textContent = item.name;
 
-        const resultDetailsContent = document.createElement("div");
-        resultDetailsContent.classList.add("search_details--content");
+          const resultDetailsPrice = document.createElement("div");
+          resultDetailsPrice.classList.add("search_details--price");
 
-        const resultDetailsTitle = document.createElement("div");
-        resultDetailsTitle.classList.add("search_details--title");
-        resultDetailsTitle.textContent = item.name;
+          const prevPrice = document.createElement("span");
+          prevPrice.classList.add("previous-price");
 
-        const resultDetailsPrice = document.createElement("div");
-        resultDetailsPrice.classList.add("search_details--price");
-        resultDetailsPrice.textContent = `${item.price}  ₾`;
+          if (item.previousPrice) {
+            resultDetailsPrice.textContent = `${item.price} ₾`;
+            resultDetailsPrice.style.color = "var(--orange-main)";
+            prevPrice.textContent = `${item.previousPrice} ₾ prev`;
+            resultDetailsPrice.appendChild(prevPrice);
+          } else {
+            resultDetailsPrice.textContent = `${item.price} ₾`;
+          }
 
-        resultDetailsContent.appendChild(resultDetailsTitle);
-        resultDetailsContent.appendChild(resultDetailsPrice);
-        resultDetailsContainer.appendChild(resultImg);
-        resultDetailsContainer.appendChild(resultDetailsContent);
-        resultDetails.appendChild(resultDetailsContainer);
-        searchResult.appendChild(resultDetails);
-
-        if (searchValue == "") {
-          resultDetails.innerHTML = "";
-        }
-      });
+          resultDetailsContent.appendChild(resultDetailsTitle);
+          resultDetailsContent.appendChild(resultDetailsPrice);
+          resultDetailsContainer.appendChild(resultImg);
+          resultDetailsContainer.appendChild(resultDetailsContent);
+          resultDetails.appendChild(resultDetailsContainer);
+          searchResult.appendChild(resultDetails);
+        });
+      }
+    } else {
     }
   });
+}
+
+// aside slider
+
+function handleAsideSlider() {
+  const asideSliderContainer = document.getElementById("aside_slider");
+  const search_conteiner = document.getElementById("#search_conteiner");
+
+  if (asideSliderContainer.style.display === "flex") {
+    asideSliderContainer.style.display = "none";
+    search_conteiner.style.display = "flex";
+    asideSliderContainer.style.transform = "translateX(0%)";
+    asideSliderContainer.innerHTML = `
+    <div class="aside_slider--input">
+        <form action="" id="search_conteiner slider-input">
+          <img src="./assets/header/main-search.svg" alt="search-logo" />
+          <input
+            id="search_input"
+            type="text"
+            placeholder="ძიება"
+            onclick="displaySearchValue()"
+          />
+          <img src="./assets/header/searchLoop.svg" alt="loop" />
+        </form>
+      </div>`;
+  } else {
+    asideSliderContainer.style.display = "flex";
+    asideSliderContainer.style.transform = "translateX(-100%)";
+
+    setTimeout(() => {
+      asideSliderContainer.style.transition = "transform 0.3s ease";
+      asideSliderContainer.style.transform = "translateX(0%)";
+    }, 10);
+  }
 }
